9cbdc978cde2a95ef9c82f2de42f39f87ce1678f
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2023-11-08 12:05:26 +0100

    adding setup

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 20 07:49:16 2017
+
+@author: Ali Aghaeifar
+"""
+
+
+from setuptools import setup
+
+
+setup(name='recotwix', # this will be name of package in packages list : pip list 
+      version='2.0',
+      description='Reconstruction utilities for siemens twix data',
+      keywords='twix,reconstruction,mri,nifti',
+      author='Ali Aghaeifar',
+      author_email='ali.aghaeifar [at] tuebingen.mpg [dot] de',
+      license='MIT License',
+      packages=['recotwix'],
+      install_requires = ['tqdm','numpy','nibabel','torch']
+     )
