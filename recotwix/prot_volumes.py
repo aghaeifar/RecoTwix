82409ba50a293976226c686e4a657584c0a01ddb
Ali Aghaeifar <ali.aghaeifar@tuebingen.mpg.de>
2024-01-16 10:55:46 +0100

    refine structure

--- recotwix/prot_volumes.py
+++ recotwix/prot_volumes.py
@@ -1,11 +1,12 @@
 import os
 import math
+from typing import Any
 import numpy as np
 from . import transformation as T
 import twixtools.twixprot as twixprot
 
 
-class volume:
+class volume_orientation:
     _norm = None # [norm_sag, norm_cor, norm_tra]
     _rot = None # inplane rotation (radians)
     _pos = None # offset from origin (mm)
@@ -16,7 +17,7 @@ class volume:
     _transformation = None    
     _name = None
 
-    def __init__(self, volume_structure=None, res=None, thickness=None, name=None, res2fov_ratio=1) -> None:
+    def __init__(self, volume_structure=None, res=None, thickness=None, name=None) -> None:
         
         v = volume_structure
         self._norm = [v['sNormal'].get('dSag',0), v['sNormal'].get('dCor',0), v['sNormal'].get('dTra',0)]
@@ -24,10 +25,16 @@ class volume:
         self._pos = [v.get('sPosition', {}).get('dSag',0), v.get('sPosition', {}).get('dCor',0), v.get('sPosition', {}).get('dTra',0)]
         self._fov = {'x':v['dReadoutFOV'], 'y':v['dPhaseFOV'], 'z':v['dThickness']}
         if res is None:
-            res = {key: int(value*res2fov_ratio) for key, value in self._fov.items()}
+            res = {key: int(value) for key, value in self._fov.items()}
+        if thickness is None:
+            thickness = self._fov['z'] / res['z']
+        # Resampling fails when a volume with res['z'] = 1 is given.
+        if res['z'] == 1:
+            res['z'] = 3
+            thickness = thickness / 3
 
         self._res = res  
-        self._thickness = thickness if thickness is not None else (self._fov['z'] / self._res['z'])
+        self._thickness = thickness
 
         dcm = T.calc_norm2dcm(self._norm[0], self._norm[1], self._norm[2], self._rot)
         self._transformation = T.calc_tranformation_matrix(dcm, self._pos)
@@ -39,6 +46,9 @@ class volume:
         vol = np.ones((self._res['y'], self._res['x'], self._res['z']))
         img = nib.Nifti1Image(vol, self._affine)
         nib.save(img, filename)
+
+    def data(self):
+        return np.ones(self.shape, dtype=np.float32)
             
     @property
     def affine(self):
@@ -57,65 +67,70 @@ class volume:
         return (self._fov['y'], self._fov['x'], self._fov['z'])
     
 
-class adjustment_volume(volume):
-    def __init__(self, xprot) -> None:
-        super().__init__(xprot['sAdjData']['sAdjVolume'], res2fov_ratio=0.5)  
+class volume():
+    _vol_box = list()
+    name = None
+
+    def __init__(self, name=None) -> None:
+        self._vol_box = list()
+        self.name = name
+
+    def add(self, vol):
+        self._vol_box.append(vol)
+
+    def __getitem__(self, index):
+        if index >= len(self._vol_box):
+            raise IndexError(f'Item out of range. {self.name} volume has only {len(self._vol_box)} items but {index} is asked.')
+        return self._vol_box[index]
+    
+    def __len__(self):
+        return len(self._vol_box)
+    
+
+class volume_adjustment(volume):
+    def __init__(self, xprot=None) -> None:
+        super().__init__('adjustment')
+        if xprot is None:
+            return
+        self.add(volume_orientation(xprot['sAdjData']['sAdjVolume']))
 
 
-class slice_volume():
-    _slice_volume = None
-
-    def __init__(self, xprot) -> None:
+class volume_slice(volume):
+    def __init__(self, xprot=None) -> None:
+        super().__init__('slice')
+        if xprot is None:
+            return
         if xprot.get('sSliceArray', None) is None:
             return
         if xprot['sSliceArray'].get('asSlice', None) is None:
             return
 
-        # transformation per slice
-        self._slice_volume = list()
         res  = {'x':xprot['sKSpace']['lBaseResolution'], 'y':xprot['sKSpace']['lPhaseEncodingLines'], 'z':xprot['sKSpace']['lPartitions']}
         res['z'] = res['z'] if xprot['sKSpace']['ucDimension'] == 4 else 1 # in case of 2D scans, lPartitions is not valid
         positions = list()
         for SlcVol in xprot['sSliceArray']['asSlice']:                      
-            self._slice_volume.append(volume(SlcVol, res=res)) 
+            self.add(volume_orientation(SlcVol, res=res)) 
             positions.append([SlcVol.get('sPosition', {}).get('dSag',0) , SlcVol.get('sPosition', {}).get('dCor',0) , SlcVol.get('sPosition', {}).get('dTra',0)])
         
-    def __getitem__(self, index):
-        if index >= len(self._slice_volume):
-            raise IndexError(f'Item out of range. Slice volume has only {len(self._slice_volume)} items but {index} is asked.')
-        return self._slice_volume[index]
-    
-    def __len__(self):
-        return len(self._slice_volume)
 
-
-class ptx_volume():
-    _ptx_volume = None
-
-    def __init__(self, xprot) -> None:
+class volume_ptx(volume):
+    def __init__(self, xprot=None) -> None:
+        super().__init__('pTx')
+        if xprot is None:
+            return
         if xprot.get('sPTXData', None) is None:
             return
         if xprot['sPTXData'].get('asPTXVolume', None) is None:
             return
         
-        self._ptx_volume = list()
         for pTxVol in xprot['sPTXData']['asPTXVolume']:            
-            self._ptx_volume.append(volume(pTxVol))  
-
-    def __getitem__(self, index):
-        if index >= len(self._ptx_volume):
-            raise IndexError(f'Item out of range. PTx volume has only {len(self._ptx_volume)} items but {index} is asked.')
-        return self._ptx_volume[index]
-    
-    def __len__(self):
-        return len(self._ptx_volume)
+            self.add(volume_orientation(pTxVol))  
 
 
 class prot_volumes:
     xprot = None
-    _slc = None
-    _ptx = None
-    _adj = None
+    _all_volumes = dict()
+    num_volumes = 0
 
     def __init__(self, param=None) -> None:
         if param is None:
@@ -138,18 +153,19 @@ class prot_volumes:
         else:
             raise ValueError(f'Unknown parameter type: {type(param)}')
         
-        self._slc = slice_volume(self.xprot)
-        self._ptx = ptx_volume(self.xprot)
-        self._adj = adjustment_volume(self.xprot)
+        self._all_volumes = dict()
+        self._all_volumes['slc'] = volume_slice(self.xprot)
+        self._all_volumes['ptx'] = volume_ptx(self.xprot)
+        self._all_volumes['adj'] = volume_adjustment(self.xprot)
+        self.num_volumes = sum([len(v) for v in self._all_volumes.values()])
 
-    @property
-    def slc(self):
-        return self._slc
 
-    @property
-    def ptx(self):
-        return self._ptx
+    def get_volume_names(self):
+        return [name for name in self._all_volumes.keys() if len(self._all_volumes[name])>0]
 
-    @property
-    def adj(self):
-        return self._adj    
+    def get(self, volume_name):
+        if volume_name in self._all_volumes:
+            return self._all_volumes[volume_name] 
+        else:
+            raise ValueError(f'Unknown volume name: {volume_name}')
+        
--- recotwix/transformation.py
+++ recotwix/transformation.py
@@ -1,7 +1,7 @@
 
 
 import numpy as np
-import scipy as sp
+from scipy import linalg, ndimage
 
 def calc_nifti_affine(transformation, fov, res, thickness):
     """
@@ -99,42 +99,38 @@ def calc_inplane_rotation(inplane_rot_rad):
     return rot_inplane
 
 
-def reslice(volume_data, volume_affine, reference_affine, reference_size, interp_oder=3, fill_value=0):
+def resample(volume_data, volume_affine, target_affine, target_size, interp_oder=3, fill_value=0):
     """
-    reslice volume_data to reference_affine and reference_size
+    resample volume_data to target_affine and target_size
 
     volume_data (np.array): 2D, 3D, or 4D data to be resliced 
     volume_affine (np.array): 4x4 transformation matrix of volume_data
-    reference_affine (np.array): 4x4 transformation matrix of target space
-    reference_size (list): size of target space
+    target_affine (np.array): 4x4 transformation matrix of target space
+    target_size (list 3x1): size of target space
     interp_oder (int): spline interpolation order
     fill_value (float): value for out of bound voxels
     """
 
     if volume_data.ndim < 2 or volume_data.ndim > 4:
         raise ValueError('input must be 2D, 3D, or 4D')
-    if len(reference_size) != 3:
-        raise ValueError('reference_size must be 3D')
-
+    if len(target_size) != 3:
+        raise ValueError('target_size must be 3D')
     # ensure input array has 4 dimensions
     while volume_data.ndim < 4:
         volume_data = np.expand_dims(volume_data, axis=-1)
 
-    grid = np.mgrid[[slice(0, i) for i in reference_size]].reshape((3, -1))
-    grid = np.vstack((grid, np.ones(grid.shape[1])))
-    T = sp.linalg.lstsq(volume_affine, reference_affine)[0] # calculate -> inv(ref_affine) @ target_affine
-    grid = T @ grid
-
+    T = linalg.lstsq(volume_affine, target_affine)[0] # calculate -> inv(ref_affine) @ target_affine
     resampled_vol = []
     for vol_data in np.moveaxis(volume_data, 3, 0):
-        print(vol_data.shape)
-        resampled_vol.append(sp.ndimage.map_coordinates(vol_data, grid[0:3,:], order=interp_oder, cval=fill_value).reshape(reference_size))
+        resampled_vol.append(ndimage.affine_transform(vol_data, T[0:3,:], output_shape=target_size, order=interp_oder, cval=fill_value))
 
-    return np.moveaxis(np.asarray(resampled_vol), 0, -1).squeeze()
+    resampled_vol = np.moveaxis(np.asarray(resampled_vol), 0, -1).squeeze()
+    return resampled_vol
 
 
-def reslice_to_standard_sapce(volume_data, volume_affine, interp_oder=3, fill_value=0):
-    reference_affine = np.diag([2, 2, 2, 1])
-    reference_affine[0:3,-1] = [-150, -150, -150]
-    reference_size = [150, 150, 150]
-    return reslice(volume_data, volume_affine, reference_affine, reference_size, interp_oder, fill_value)
+def resample_to_standard_sapce(volume_data, volume_affine, interp_oder=3, fill_value=0):
+    fov = 300   # mm
+    res = 1.5   # mm
+    target_size   = [fov/res]*3
+    target_affine = np.hstack((np.eye(4,3)*res, np.array([-fov/2,-fov/2,-fov/2,1]).reshape(4,1)))
+    return resample(volume_data, volume_affine, target_affine, target_size, interp_oder, fill_value)
